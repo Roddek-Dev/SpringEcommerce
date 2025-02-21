1ae811c2fa188848bf955fdee2da9bd7d3449cd9
Roddek-Dev <Rodyavila04@gmail.com>
2025-02-21 12:57:24 -0500

    v0.17_vistaUsuariosOrdenesDetalleOrdenesEnTemplateAdminController&templateAdminReportesAdmin_21022024_Roddek

--- src/main/java/com/sena/ecommerce/controller/administradorController.java
+++ src/main/java/com/sena/ecommerce/controller/administradorController.java
@@ -1,30 +1,66 @@
 package com.sena.ecommerce.controller;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.sena.ecommerce.model.Orden;
 import com.sena.ecommerce.model.Producto;
+import com.sena.ecommerce.service.IOrdenService;
 import com.sena.ecommerce.service.IProductoService;
+import com.sena.ecommerce.service.IUsuarioService;
 
 @Controller
 @RequestMapping("/administrador")
 public class administradorController {
 
+	private final Logger LOGGER = (Logger) LoggerFactory.getLogger(administradorController.class);
+
 	@Autowired
 	private IProductoService productoService;
+
+	@Autowired
+	private IUsuarioService usuarioService;
+
+	@Autowired
+	private IOrdenService ordenService;
 
 	@GetMapping("")
 	public String home(Model model) {
 		List<Producto> productos = productoService.findAll();
 		model.addAttribute("productos", productos);
 		return "administrador/home";
+	}
 
+	// Metodo de usuarios
+	@GetMapping("/usuarios")
+	public String usuarios(Model model) {
+		model.addAttribute("usuarios", usuarioService.findAll());
+		return "administrador/usuarios";
+	}
+
+	// metodo de las ordenes
+	@GetMapping("/ordenes")
+	public String ordenes(Model model) {
+		model.addAttribute("ordenes", ordenService.findAll());
+		return "administrador/ordenes";
+	}
+
+	// metodo de las ordenes
+	@GetMapping("/detalle/{id}")
+	public String detalleOrden(Model model, @PathVariable Integer id) {
+		LOGGER.info("Id de la orden: {}", id);
+		Optional<Orden> orden = ordenService.findById(id);
+		model.addAttribute("detalles", orden.get().getDetalle());
+		return "administrador/detalleOrden";
 	}
 
 }
